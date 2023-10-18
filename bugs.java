d87d4a7849c448ad3c0e7acbe38f6cd64040f352
shivani2190 <shivanibrajesh222@gmail.com>
2023-10-18 08:23:46 +0530

    bugs code updated

---
+++ bugs.java
@@ -0,0 +1,47 @@
+public class bugs{
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        if (m > n) {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+            int tempLen = m;
+            m = n;
+            n = tempLen;
+        }
+        int iMin = 0, iMax = m;
+        int halfLen = (m + n + 1) / 2;
+        double maxOfLeft, minOfRight;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = halfLen - i;
+            if (i < m && nums2[j - 1] > nums1[i]) {
+                iMin = i + 1;
+            } else if (i > 0 && nums1[i - 1] > nums2[j]) {
+                iMax = i - 1;
+            } else {
+                if (i == 0) {
+                    maxOfLeft = nums2[j - 1];
+                } else if (j == 0) {
+                    maxOfLeft = nums1[i - 1];
+                } else {
+                    maxOfLeft = Math.max(nums1[i - 1], nums2[j - 1]);
+                }
+                if ((m + n) % 2 == 1) {
+                    return maxOfLeft;
+                }
+                if (i == m) {
+                    minOfRight = nums2[j];
+                } else if (j == n) {
+                    minOfRight = nums1[i];
+                } else {
+                    minOfRight = Math.min(nums1[i], nums2[j]);
+                }
+                return (maxOfLeft + minOfRight) / 2;
+            }
+        }
+        return 0; // In case the input arrays are not sorted
+    }
+}
+
